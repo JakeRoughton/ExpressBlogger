ed2b1f990985450d03baf32712da77c888b17ad4
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2023-02-02 14:41:01 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -51,8 +51,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// app.listen(port, () => {
-//   console.log(`ExpressBlogger app listening on port ${port}`)
-// })
+
 
 module.exports = app;
