61d6ae9f99c781a7d25d9c0181be259c3755aa16
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2023-02-14 10:36:55 -0500

    Add files via upload

--- blogs.js
+++ blogs.js
@@ -1,65 +1,138 @@
-//Create variable to hold function data for validation
-const validateblogs = (blogs) => {
-    //Blog title is required and must be string
-        if(blogs.title === undefined || typeof(blogs.title) !=="string"){
-            return {
-                isValid: true,
-                message: "Title is required and must be a string"
-        }
-    }
-    //Blog text is required and must be string
-        if (blogs.text === undefined || typeof(blogs.text) !== "string"){
-            return {
-                isValid: true,
-                message: "Text is required and must be a string"
-        }
-    }
-    //Blog author is required and must be string
-        if (blogs.author === undefined || typeof(blogs.author) !== "string"){
-            return {
-                isValid: true,
-                message: "Author is required and must be a string"
-        }
-    }
+const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const router = express.Router();
 
-    //Create variable to hold blogs title that is a string
-    const blogTitle = blogs.title.filter((title)=>{
+//instantiate mongodb 
+const { db } = require('../mongo');
 
-        if(typeof(title) !== "string"){
-            return true
-    }   else{
-            return false
-        }
-    })
-    //Blog title must not exceed 40 characters
-        if (blogTitle > 40){
-            return{
-                isValid: false,
-                message: "Blog title needs to be less than 40 characters"
-        }
-    }
-        //Create variable to hold blogs author that is a string
-    const blogAuthor = blogs.author.filter((author)=>{
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  const blogs = await db()
+  .collection('sample_blogs')
+  .find({})
+  .limit(5)
+  .toArray(function(err, result){
+      if (err) {
+        res.status(400).send("error fetching blogs")
+      } else {
+        res.json(result);
+      }
+    }); 
+
+    res.json({
+      success:true,
+      blogs: blogs
+    });
     
-        if(typeof(author) !== "string"){
-            return true
-        } else{
-            return false
+});
+
+//GET all blogs from database
+router.get('/all', async function(req, res) {
+    const blogs = await db()
+    .collection('sample_blogs')
+    .find({})
+    .limit(5)
+    .toArray(function(err, result){
+        if (err) {
+          res.status(400).send("error fetching blogs")
+        } else {
+          res.json(result);
         }
+      }); 
+  
+      res.json({
+        success:true,
+        blogs: blogs
+      });
+      
+  });
+
+//GET one blog from database
+  router.get('/get-one', async function(req, res) {
+    const blogs = await db()
+    .collection('sample_blogs')
+    .find({})
+    .limit(1)
+    .toArray(function(err, result){
+        if (err) {
+          res.status(400).send("error fetching blogs")
+        } else {
+          res.json(result);
+        }
+      }); 
+  
+      res.json({
+        success:true,
+        blogs: blogs
+      });
+      
+  });
+
+//GET one blog from title search
+  router.get('/get-one/:titleToFind', async function(req, res) {
+    const blogs = await db()
+    .collection('sample_blogs')
+    .findOne({
+        title:req.params.titleToFind
     })
-        //Blog author must not exceed 40 characters
-        if (blogAuthor > 40){
-            return{
-                isValid: false,
-                message: "Blog author needs to be less than 40 characters"
-            }
-        }
+      res.json({
+        success:true,
+        blogs: blogs
+      });
+  });
 
-    return{
-        isValid: true
-    }
+//POST new blog
+  router.post('/create-one', async function(req, res, next) {
+    const blogs = await db()
+    .collection('sample_blogs')
+    .insertOne({    
+        title: req.body.title,
+        text: req.body.text,
+        author: req.body.author,
+        category: req.body.category,
+        last_modified: new Date(),
+        })
 
-}
-module.exports = {
-    validateblogs
-}
+      res.json({
+        success:true,
+        blogs: blogs
+      });
+  });
+
+//  STRETCH GOALS :
+//get-multi/ (GET): get multiple sorted results
+//HINT: use find().sort() 
+router.get('/get-multi', async function(req, res, next) {
+  // console.log(req.params.opt1)
+  const blogs = await db()
+  .collection('sample_blogs')
+  .find({title: { $in: [req.query.opt1,req.query.opt2,req.query.opt3]}})
+  .sort(
+    {title: 1, blogs: 1}
+  )
+  .toArray()
+
+    res.json({
+      success:true,
+      blogs: blogs
+    });
+})
+
+
+
+//delete-multi/ (GET): get multiple results
+//HINT: use deleteMany()
+
+router.get('/get-one/:titleToFind', async function(req, res) {
+  const blogs = await db()
+  .collection('sample_blogs')
+  .findOne({
+      title:req.params.titleToFind
+  })
+    res.json({
+      success:true,
+      blogs: blogs
+    });
+})
+  
+module.exports = router;
